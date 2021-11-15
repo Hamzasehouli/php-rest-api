58800dc98a63512f299aaee286a3d5cb9206bc5e
hamzasehouli <sehoulihamza@yahoo.com>
2021-11-15 10:10:28 +0100

    Setup the index.php file

--- Router.php
+++ Router.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace app;
+
 class Router
 {
     private $getRoutes;
--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
+use app\Router;
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Credentials: true');
+header('content-type:application/json');
+
+$router = new Router();
+$router->get('/api/v1/auth', [UserController::class, 'getAllUsers']);
+$router->post('/api/v1/auth', [UserController::class, 'creatUser']);
+$router->post('/api/v1/auth/signup', [UserController::class, 'signup']);
+$router->post('/api/v1/auth/login', [UserController::class, 'login']);
+$router->call();
