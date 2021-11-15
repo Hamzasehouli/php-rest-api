eb24030dc22a40d22b44a61b64412c44481f69e2
hamzasehouli <sehoulihamza@yahoo.com>
2021-11-15 16:50:30 +0100

    Added protect controller

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -34,20 +34,18 @@ class UserController
             $stmt1 = $User->findOne();
             $stmt1->bindValue(':username', $data->username);
             $stmt1->execute();
-            $user1 = $stmt1->fetch();
+            $user1 = $stmt1->fetch(\PDO::FETCH_ASSOC);
+            extract($user1);
 
             $gt = new GenerateJwt();
-            $jwt = $gt->generateToken($user1->id);
+            $jwt = $gt->generateToken($id);
             print_r(json_encode(['status' => 'success', 'message' => 'You signed up successfully', 'token' => $jwt]));
         } else {
             print_r(json_encode(['status' => 'success', 'message' => 'Something went wrong']));
         }
 
     }
-    // public static function signup()
-    // {
-    //     echo 'signup';
-    // }
+
     public static function login()
     {
         $User = new UserModel();
@@ -81,6 +79,59 @@ class UserController
         // if ($stmt->execute()) {
         //     print_r(json_encode(['status' => 'success', 'message' => 'You signed up successfully']));
         // }
+    }
+
+    public static function protect()
+    {
+        $d = json_decode(file_get_contents('php://input'), true);
+        if (isset($d['jwt'])) {
+            print_r($d);
+            $resp = explode('=', $d['jwt']);
+            $jwt = $resp[1];
+        } else {
+            extract($_COOKIE);
+            print_r($jwt);
+        }
+        // split the jwt
+        $tokenParts = explode('.', $jwt);
+        $header = base64_decode($tokenParts[0]);
+        $payload = base64_decode($tokenParts[1]);
+        $signature_provided = $tokenParts[2];
+
+        // check the expiration time - note this will cause an error if there is no 'exp' claim in the jwt
+        $expiration = json_decode($payload)->exp;
+        $is_token_expired = ($expiration - time()) < 0;
+
+        // build a signature based on the header and payload using the secret
+        // Encode Header to Base64Url String
+        $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+
+        // Encode Payload to Base64Url String
+        $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+        // Create Signature Hash
+        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, 'abC123!', true);
+
+        // Encode Signature to Base64Url String
+        $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+
+        // verify it matches the signature provided in the jwt
+        $is_signature_valid = ($base64UrlSignature === $signature_provided);
+        // $isTokenValid = null;
+        // if ($is_token_expired || !$is_signature_valid) {
+        //     $isTokenValid = false;
+        // } else {
+        //     $isTokenValid = true;
+        // }
+        print_r($is_signature_valid);
+
+        if ($is_signature_valid) {
+            header("HTTP/1.1 200");
+            return print_r(json_encode(['status' => 'success', 'message' => 'You logged in successfully', 'isLoggedin' => true]));
+        } else {
+            header("HTTP/1.1 403");
+            return print_r(json_encode(['status' => 'fail', 'message' => 'your ere not logged in to perform the task', 'isLoggedin' => false]));
+
+        }
     }
 
 }
--- index.php
+++ index.php
@@ -17,5 +17,6 @@ $router = new Router();
 $router->get('/api/v1/auth', [UserController::class, 'getAllUsers']);
 $router->post('/api/v1/auth/signup', [UserController::class, 'signup']);
 $router->post('/api/v1/auth/login', [UserController::class, 'login']);
+$router->post('/api/v1/auth/protect', [UserController::class, 'protect']);
 // $router->post('/api/v1/auth', [UserController::class, 'createUser']);
 $router->call();
